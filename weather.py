b9622f295a310bdd81a7bc28db500341b72fa4f1
joonaskiuru <joonas.kiuru@gmail.com>
2024-04-02 16:51:57 +0300

    Add: --log-level=3 to chrome options

--- weather.py
+++ weather.py
@@ -43,6 +43,7 @@ class App(ctk.CTk):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('headless')
         chrome_options.add_experimental_option('excludeSwitches',['enable-logging'])
+        chrome_options.add_argument('--log-level=3')
         return chrome_options
 
     # Validate Url
